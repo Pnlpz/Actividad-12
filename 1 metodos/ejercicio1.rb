2762a0268bcf873886163b48dd0b9fdb6b4378fc
Marisol Smith <marisolsmith@gmail.com>
2018-05-01 23:20:38 -0300

    archivos

---
+++ 1 metodos/ejercicio1.rb
@@ -0,0 +1,18 @@
+# Optimiza el código utilizando tu editor de texto.
+# hint: Debes utilizar 'rubocop'
+# y no obtener errores ni warnings.
+
+def par(x)
+  if x % 2 == 0
+    return true
+  else
+    return false
+  end
+end
+
+puts par(2)
+puts par(3)
+puts par(4)
+puts par(5)
+
+#No tengo Rubocop. Lo instalé, pero no sé dónde activarlo.
