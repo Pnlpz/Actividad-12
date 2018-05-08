e0723b84d951acb4fc6ebbd20d61b94047bf7262
Marisol Smith <marisolsmith@gmail.com>
2018-05-08 20:47:24 -0300

    vamos entendiendo

--- 1 metodos/ejercicio2.rb
+++ 1 metodos/ejercicio2.rb
@@ -1,18 +1,15 @@
 # El siguiente programa debería mostrar sí o no, sin embargo muestrar error
 # Se pide identificar el error y corregirlo.
 
-def random
+def random ()
   result = [true, false].sample
   puts result
-
+  if result == true
+    print 'sí'
+  else
+    print 'no'
+  end
 end
 
-print random
-
-if random == true
-  print 'sí'
-else
-  print 'no'
-end
-
-#qué se cambia? No entiendo
+random
+#Listo
